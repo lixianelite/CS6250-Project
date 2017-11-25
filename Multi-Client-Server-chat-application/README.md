5404482d818f0a297355d0956c23824b8baf013f
xli684 <lixianelite@gatech.edu>
2017-11-25 16:01:14 -0500

    old files moved to oldVersion directory

--- Multi-Client-Server-chat-application/README.md
+++
@@ -1,9 +0,0 @@
-To see how to run app : https://www.youtube.com/watch?v=LwK3UAyrXiQ
-
-
-you can find more info on : https://aboullaite.me/socket-programming-java-swing-android/
-
-have fun !!
-
-sdfasdfa
-
---
+++ oldVersion/project_StartUp/infra/UserObj.java
@@ -0,0 +1,77 @@
+package infra;
+
+
+public class UserObj extends PacketObj {
+
+    String account;
+    String userName;
+    String password;
+
+    public UserObj(String account, String userName, String password) {
+        this.account = account;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public UserObj() {
+        account = "";
+        password = "";
+        userName = "";
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String parse(){
+        StringBuilder sb = new StringBuilder();
+
+        if(account.length()==0)
+            sb.append("nil");
+        else
+            sb.append(account);
+
+        sb.append("#");
+
+        if(userName.length()==0)
+            sb.append("nil");
+        else
+            sb.append(userName);
+
+        sb.append("#");
+
+        if(password.length()==0)
+            sb.append("nil");
+        else
+            sb.append(password);
+
+        return sb.toString();
+    }
+
+    @Override
+    public void deparse(String packet){
+
+        String[] arr=packet.split("#");
+
+        if(arr[0]=="nil")
+            account=null;
+        else
+            account=arr[0];
+
+        if(arr[1]=="nil")
+            userName=null;
+        else
+            userName=arr[1];
+
+        if(arr[2]=="nil")
+            password=null;
+        else
+            password=arr[2];
+    }
+
+}
