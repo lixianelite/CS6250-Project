65b2dbaedface4ccdcdf059005eb70c3d1dd2743
Yinglin Li <allamlee106@gmail.com>
2017-12-10 17:32:52 -0500

    Add room server

--- newVersion/src/BackEnd/Server.java
+++ newVersion/src/BackEnd/Server.java
@@ -8,12 +8,15 @@ import java.io.IOException;
 public class Server {
     private static LoginServer loginSystem;
     private static ServerEnd chatSystem;
+    private static RoomServer roomSystem;
     public static int loginPort = 2000;
     public static int chatPort = 2001;
+    public static int roomPort = 2002;
 
     public static void main(String args[]) {
         loginSystem = new LoginServer(loginPort);
         chatSystem = new ServerEnd(chatPort);
+        roomSystem = new RoomServer(roomPort);
 
         Thread thread1 = new Thread() {
             @Override
@@ -31,12 +34,22 @@ public class Server {
             }
         };
 
+        Thread thread3 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("start rooms server");
+                roomSystem.runServer();
+            }
+        };
+
         thread1.start();
         thread2.start();
+        thread3.start();
 
         try {
             thread1.join();
             thread2.join();
+            thread3.join();
         }
         catch (Exception e) {
             System.out.println("error in servers: " + e);
